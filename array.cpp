379323d8a107c10d40f7e4e9a4d8d7033a7d7789
piyanshi06 <piyanshibawanthade@gmail.com>
2026-09-08 16:02:00 +0530

    Create array.cpp
    Add first array program

---
+++ array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[6] = {10, 25, 7, 40, 18, 30};
+
+    for (int i = 0; i < 6; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
